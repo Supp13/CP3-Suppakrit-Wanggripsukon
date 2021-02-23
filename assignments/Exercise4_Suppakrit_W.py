a9975bef763d1db7a224cc878c523b4e3d4b7786
Supp13 <suppakrit2006@gmail.com>
2021-02-24 06:15:41 +0700

    Added Exercise4_Suppakrit_W file
    Added Exercise4_Suppakrit_W file

---
+++ assignments/Exercise4_Suppakrit_W.py
@@ -0,0 +1,9 @@
+Foundation_English = 69.6
+General_Business = 85.2
+Introduction_to_Computer_Systems = 45.6
+Computer_Programming = 98.6
+print('--- Your Score ---')
+print(f'Foundation English : {Foundation_English}')
+print(f'General Business : {General_Business}')
+print(f'Introduction to Computer Systems : {Introduction_to_Computer_Systems}')
+print(f'Computer Programming : {Computer_Programming}')
