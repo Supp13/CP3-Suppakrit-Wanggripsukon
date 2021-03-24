e50e9318ee8b9af61b2cc49a4aa24b42f148ed55
Supp13 <suppakrit2006@gmail.com>
2021-03-24 22:16:05 +0700

    Added Lecture 50 homework
    Added Lecture 50 homework

---
+++ assignments/Lecture50_Suppakrit_W.py
@@ -0,0 +1,15 @@
+def plusNumber(x, y):
+    print(x + y)
+def minusNumber(x, y):
+    print(x - y)
+def multiplyNumber(x, y):
+    print(x * y)
+def divideNumber(x, y):
+    print(x / y)
+
+x = int(input("Input your first number: "))
+y = int(input("Input your second number: "))
+plusNumber(x, y)
+minusNumber(x, y)
+multiplyNumber(x, y)
+divideNumber(x, y)
